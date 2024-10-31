4ad85881952911753e046775bf380419ea7b10b3
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-31 18:59:09 +0100

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-/*Aggiungiamo la componente js di interazione con l’utente.
-Quando l’utente fa click sul bottone submit del form, il sito deve calcolare l’ammontare del
-preventivo per le ore di lavoro richieste.
-Il prezzo finale è dato dal numero di ore per prezzo orario. Supponiamo per semplicità che
-ogni progetto richieda lo stesso numero di ore di lavoro (es: 10 ore).
-Il prezzo orario per una commissione varia in questo modo:
-● se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50€/l’ora
-● se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.30€/l’ora
-● se la commissione riguarda l’analisi progettuale il prezzo orario è di 33.60€/l’ora
-L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti: YHDNU32,
-JANJC63, PWKCN25, SJDPO96, POCIE24.
-Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul
-prezzo finale. Se il codice inserito non è valido, il sito deve informare l’utente che il codice
-non è valido e il prezzo finale viene calcolato senza applicare sconti.
-Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2
-decimali e il simbolo dell’euro). */
-
 const form = document.querySelector("form");
 const nameForm = document.getElementById("inputNome");
 const cognomeForm = document.getElementById("inputCognome");
@@ -69,7 +52,7 @@ form.addEventListener("submit", function (event) {
    }
 
    else {
-      printError(validateName(nameForm.value), validateName(cognomeForm.value), validateSelect(job.value), validateEmail(email.value), privacyChecked.checked);
+      printError(validateName(nameForm.value), validateName(cognomeForm.value), validateSelect(job.value), validateEmail(email.value), checkPrivacy.checked);
    }
 
 });
@@ -125,7 +108,6 @@ function validateName(name) {
  * @returns {boolean}
  */
 function validateSelect(selectValue) {
-   let isValid = true;
 
    /* if (selectValue === "backend" || selectValue === "frontend" || selectValue === "analysis") {
       return isValid;
@@ -137,12 +119,12 @@ function validateSelect(selectValue) {
 
    for (let i = 0; i < jobsArray.length; i++) {
 
-      if (selectValue === jobsArray[i].job)
-         return isValid;
+      if ( selectValue === jobsArray[i].job )
+         return true;
 
    }
 
-   return isValid;
+   return false;
 
 }
 
